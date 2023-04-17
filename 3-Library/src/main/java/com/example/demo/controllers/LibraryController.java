abfdd5a3485f14a8f1fd491300b9326d278fff47
carlosxmerca <eduarmercado4@gmail.com>
2023-04-17 09:43:54 -0600

    update books and DTOvalidations

--- 3-Library/src/main/java/com/example/demo/controllers/LibraryController.java
+++ 3-Library/src/main/java/com/example/demo/controllers/LibraryController.java
@@ -2,15 +2,24 @@ package com.example.demo.controllers;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.models.entities.*;
+
+import jakarta.validation.Valid;
+
 import com.example.demo.models.dtos.*;
 
 @Controller
@@ -27,21 +36,64 @@ public class LibraryController {
         library.add(new Book("843760494X", "Cien Años de Soledad"));
         library.add(new Book("0553804367", "A Briefer History of Time!!"));
     }
-    
-    @GetMapping("/all")
-    public String getBookList(Model model) {
-    	model.addAttribute("books", library);
-    	return "book-list";
-    }
-    
+        
     @PostMapping("/save")
-    public String saveBook(@ModelAttribute SaveBookDTO bookInfo) {
+    public String saveBook(@ModelAttribute @Valid SaveBookDTO bookInfo,
+    		BindingResult validations, Model model) {
+    	
+    	String time = Calendar.getInstance().getTime().toString();
+    	model.addAttribute("time", time);
+    	
+    	HashMap<String, List<String>> errorMap = new HashMap<>();
+    	
+    	if (validations.hasErrors()) {
+    		System.out.println("Hay errores");
+    		
+    		// Show errors in console
+    		List<FieldError> errors = validations.getFieldErrors();
+    		errors.forEach((error) -> {
+    			System.out.println(error.getField() + ": " + error.getDefaultMessage());
+    		});
+    		
+    		if (!errors.isEmpty())
+    			model.addAttribute("hasErrors", true);
+    		
+    		// Map errors to display in UI
+    		validations.getFieldErrors().stream().forEach(error -> {
+    			String key = error.getField();
+    			
+    			List<String> itemErrors = errorMap.get(key);
+    			if (itemErrors == null)
+    				itemErrors = new ArrayList<>();
+    			
+    			itemErrors.add(error.getDefaultMessage());
+    			errorMap.put(key, itemErrors);
+    		});
+    		model.addAllAttributes(errorMap);
+    		
+    		
+    		return "main";
+    	}
+    	
     	System.out.println(bookInfo);
     	
     	Book newBook = new Book(bookInfo.getIsbn(), bookInfo.getTitle());
     	library.add(newBook);
     	
-    	return "redirect:/";
+    	return "redirect:/all";
+    }
+    
+    @PostMapping("/update")
+    public String updateBook(@ModelAttribute SaveBookDTO bookInfo) {
+    	
+    	Book book = library.stream().filter(b -> b.getIsbn().equals(bookInfo.getIsbn())).findFirst().orElse(null);
+    	
+    	if (book != null) {
+    		book.setTitle(bookInfo.getTitle());
+    		System.out.println(book);
+    	}
+    	
+    	return "redirect:/all";
     }
     
     @GetMapping("/")
@@ -50,6 +102,25 @@ public class LibraryController {
     	model.addAttribute("time", time);
     	
     	return "main";    
+    }
+    
+    @GetMapping("/all")
+    public String getBookList(Model model) {
+    	model.addAttribute("books", library);
+    	return "book-list";
+    }
+    
+    @GetMapping("/book/{isbn}")
+    public String getUpdatePage(@PathVariable String isbn, Model model) {
+    	String time = Calendar.getInstance().getTime().toString();
+    	model.addAttribute("time", time);
+    	
+    	Book defaultBook = new Book("843760494X", "Cien Años de Soledad");
+    	Book book = library.stream().filter(b -> b.getIsbn().equals(isbn)).findFirst().orElse(defaultBook);
+    	
+    	model.addAttribute("bookTitle", book.getTitle());
+    	
+    	return "update-book";    
     }
     
 }
--- 3-Library/src/main/java/com/example/demo/models/dtos/SaveBookDTO.java
+++ 3-Library/src/main/java/com/example/demo/models/dtos/SaveBookDTO.java
@@ -1,12 +1,24 @@
 package com.example.demo.models.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class SaveBookDTO {
+	@NotEmpty(message = "ISBN vacio")
+	@Size(min = 10, max = 10, message = "Debe tener 10 caracteres")
 	private String isbn;
+	
+	@NotEmpty(message = "Titulo vacio")
 	private String title;
+	
+	@NotEmpty(message = "Dueño vacio")
+	@Email(message = "Formato de dueño incorrecto")
+	// @Pattern(regexp = "")
 	private String owner;
 }
