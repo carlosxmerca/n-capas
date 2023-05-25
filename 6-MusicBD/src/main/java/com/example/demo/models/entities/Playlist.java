a1a1b35cc360cd55d6fd25fa17dc3a17a44dc837
carlosxmerca <eduarmercado4@gmail.com>
2023-05-24 19:54:45 -0600

    Update main entities and add utils

--- 6-MusicBD/src/main/java/com/example/demo/models/entities/Playlist.java
+++ 6-MusicBD/src/main/java/com/example/demo/models/entities/Playlist.java
@@ -3,13 +3,18 @@ package com.example.demo.models.entities;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+import java.util.List;
 import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Data
 @NoArgsConstructor
 @Entity
 @Table(name = "playlists")
+@ToString(exclude = {"songxplaylist"})
 public class Playlist {
     @Id
     @Column(name = "code")
@@ -24,6 +29,10 @@ public class Playlist {
 
     @Column(name = "user_code")
     private UUID userCode;
+    
+    @OneToMany(mappedBy = "playList", fetch = FetchType.LAZY)
+	@JsonIgnore
+	private List<SongXPlaylist> songxplaylist;
 
     public Playlist(String title, String description, UUID userCode) {
         super();
