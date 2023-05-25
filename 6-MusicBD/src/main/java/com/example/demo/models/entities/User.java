57962e90088b013fbeca1a0e82f83b05659ade1f
carlosxmerca <eduarmercado4@gmail.com>
2023-05-24 20:08:57 -0600

    Update user entity

--- 6-MusicBD/src/main/java/com/example/demo/models/entities/User.java
+++ 6-MusicBD/src/main/java/com/example/demo/models/entities/User.java
@@ -1,22 +1,27 @@
 package com.example.demo.models.entities;
 
+import java.util.List;
 import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @NoArgsConstructor
 @Entity
 @Table(name = "user")
+@ToString(exclude = {"playLists"})
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -33,6 +38,10 @@ public class User {
 	@Column(name = "password")
 	@JsonIgnore
     private String password;
+	
+	@OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+	@JsonIgnore
+	private List<Playlist> playLists;
 	
 	public User(String username, String email, String password) {
 		super();
