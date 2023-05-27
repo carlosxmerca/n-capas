22dd7b2b3a396260ae034141b6b3a3683a7155fb
carlosxmerca <eduarmercado4@gmail.com>
2023-05-27 16:35:51 -0600

    Update Song Controller :rocket:

--- 6-MusicBD/src/main/java/com/example/demo/controllers/SongController.java
+++ 6-MusicBD/src/main/java/com/example/demo/controllers/SongController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,14 +10,20 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.models.dtos.response.MessageDTO;
 import com.example.demo.models.dtos.songs.SaveSongDTO;
 import com.example.demo.models.dtos.songs.UpdateSongDTO;
+import com.example.demo.models.entities.Song;
+import com.example.demo.services.SongService;
 import com.example.demo.utils.RequestErrorHandler;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/songs")
@@ -23,42 +31,81 @@ import com.example.demo.utils.RequestErrorHandler;
 public class SongController {
 	
 	@Autowired
+	private SongService songService;
+	@Autowired
 	private RequestErrorHandler errorHandler;
 	
 	@GetMapping("")
 	public ResponseEntity<?> getSongs() {
-		return new ResponseEntity<>("songs", HttpStatus.OK);
+		List<Song> songs = songService.findAll();
+		return new ResponseEntity<>(songs, HttpStatus.OK);
 	}
 	
-	@GetMapping("/{id}")
-	public ResponseEntity<?> getSong() {
-		return new ResponseEntity<>("song", HttpStatus.OK);
+	@GetMapping("/{title}")
+	public ResponseEntity<?> getSongByTitle(@PathVariable String title) {
+		Song song = songService.findOneByTitle(title);
+		if (song == null)
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		
+		return new ResponseEntity<>(song, HttpStatus.OK);
+	}
+	
+	@GetMapping("/code/{code}")
+	public ResponseEntity<?> getSongByCode(@PathVariable String code) {
+		Song song = songService.findOneById(code);
+		if (song == null)
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		
+		return new ResponseEntity<>(song, HttpStatus.OK);
 	}
 	
 	@PostMapping("")
 	public ResponseEntity<?> createSong(
-			@ModelAttribute SaveSongDTO data, BindingResult validations) {
+			@ModelAttribute @Valid SaveSongDTO data, BindingResult validations) {
 		if (validations.hasErrors()) {
 			return new ResponseEntity<>(
 					errorHandler.mapErrors(validations.getFieldErrors()), HttpStatus.BAD_REQUEST);
 		}
 		
-		return new ResponseEntity<>(HttpStatus.CREATED);
+		try {
+			songService.save(data);
+			return new ResponseEntity<>(new MessageDTO("song created"), HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 	
 	@PutMapping("")
 	public ResponseEntity<?> updateSong(
-			@ModelAttribute UpdateSongDTO data, BindingResult validations) {
+			@ModelAttribute @Valid UpdateSongDTO data, BindingResult validations) {
 		if (validations.hasErrors()) {
 			return new ResponseEntity<>(
 					errorHandler.mapErrors(validations.getFieldErrors()), HttpStatus.BAD_REQUEST);
 		}
 		
-		return new ResponseEntity<>(HttpStatus.OK);
+		Song song = songService.findOneById(data.getCode());
+		if (song == null)
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		
+		try {
+			songService.update(data);
+			return new ResponseEntity<>(new MessageDTO("song updated"), HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 	
-	@DeleteMapping("/{id}")
-	public ResponseEntity<?> deleteSong() {
-		return new ResponseEntity<>("deleted song", HttpStatus.OK);
+	@DeleteMapping("/{code}")
+	public ResponseEntity<?> deleteSong(@PathVariable String code) {
+		Song song = songService.findOneById(code);
+		if (song == null)
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			
+		try {
+			songService.deleteOneById(code);
+			return new ResponseEntity<>(new MessageDTO("deleted song"), HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 }
